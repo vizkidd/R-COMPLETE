d1f3d199b5afe376a63775138a8259b7bfb4fcd6
Vishvesh <vishveshkarthik@gmail.com>
2022-09-23 14:12:33 +0200

    Delete pickle_parser.py
    Deleting old pipeline

--- pickle_parser.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def read_pickle_file(file):
-    pickle_data = pickle.load(file)
-    return pickle_data
-
-def write_pickle_file(a, file):
-	with open(file, 'wb') as handle:
-    	pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
